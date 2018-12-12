9bb26dd54816f2bb036fa661234de9b1dde430a8
John Pham <johnphammail@gmail.com>
2018-12-11 19:11:25 -0800

    renaming header

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './Header'
+import Header from './header'
 import Footer from './Footer'
 import './layout.css'
 import styled from 'styled-components'
