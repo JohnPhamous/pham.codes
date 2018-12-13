68280658575b359f7fa84aa3d66071ba5b1f1f95
John Pham <johnphammail@gmail.com>
2018-12-12 16:11:22 -0800

    adding vim keybinds for normal and insert mode

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,31 +17,69 @@ const FooterItem = styled.span`
 `
 
 const Mode = styled(FooterItem)`
-  background: rgb(49, 6, 110);
+  background: ${props => props.background};
+  font-weight: 800;
 `
 
-// const Size = styled(FooterItem)`
-//   background: rgb(139, 72, 0);
-//   margin-left: auto;
-// `
+const Size = styled(FooterItem)`
+  background: rgb(139, 72, 0);
+  margin-left: auto;
+  cursor: pointer;
+  @media (max-width: 600px) {
+    display: none;
+  }
+`
 
 const Date = styled(FooterItem)`
-  margin-left: auto;
+  /* margin-left: auto; */
   background: rgb(53, 63, 92);
   @media (max-width: 600px) {
     display: none;
   }
 `
 
-const Footer = () => {
-  return (
-    <FooterWrapper>
-      <Mode>NORMAL</Mode>
-      <FooterItem>pham.html</FooterItem>
+export default class Footer extends React.Component {
+  state = {
+    isEditing: false,
+  }
 
-      {/* <Size>12kb</Size> */}
-      <Date>Last Updated: 12/11/2018</Date>
-    </FooterWrapper>
-  )
+  toggleEdit = () => {
+    if (this.state.isEditing) {
+      document.body.contentEditable = 'false'
+      document.designMode = 'off'
+    } else {
+      document.body.contentEditable = 'true'
+      document.designMode = 'on'
+    }
+    this.setState({ isEditing: !this.state.isEditing })
+  }
+
+  componentDidMount = () => {
+    document.addEventListener('keydown', e => {
+      if (e.keyCode === 73 || e.keyCode === 27) {
+        this.toggleEdit()
+      }
+    })
+  }
+
+  componentWillUnmount = () => {
+    document.removeEventListener('keydown', e => {}, false)
+  }
+
+  render() {
+    const { isEditing } = this.state
+    return (
+      <FooterWrapper>
+        {isEditing ? (
+          <Mode background="rgb(153, 0, 0)">INSERT</Mode>
+        ) : (
+          <Mode background="rgb(49, 6, 110)">NORMAL</Mode>
+        )}
+        <FooterItem>pham.html</FooterItem>
+
+        <Size onClick={this.toggleEdit}>Toggle INSERT Mode</Size>
+        <Date>Last Updated: 12/11/2018</Date>
+      </FooterWrapper>
+    )
+  }
 }
-export default Footer
