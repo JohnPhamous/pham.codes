5f1a98d13fc25a2c0113c2de1bf3a532586af69e
John Pham <johnphammail@gmail.com>
2019-06-13 20:13:55 -0700

    Add writing

---
+++ src/components/WritingSection.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const ListContainer = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0px;
+`
+
+const SectionTitle = styled.h2`
+  margin-top: 0px;
+  margin-bottom: 0px;
+`
+
+const ListItem = styled.li`
+  &.new::before {
+    content: '+';
+    color: rgb(51, 255, 51);
+    margin-right: -8px;
+    left: -20px;
+    position: relative;
+  }
+
+  &.primary::before {
+    content: '>>>';
+    color: rgb(51, 255, 51);
+    margin-right: -28px;
+    left: -40px;
+    position: relative;
+  }
+
+  &.old::before {
+    content: '-';
+    color: rgb(255, 51, 51);
+    margin-right: -8px;
+    left: -20px;
+    position: relative;
+  }
+
+  &.old {
+    span,
+    li {
+      color: rgb(185, 180, 172) !important;
+    }
+  }
+
+  &:hover {
+    background: #3c3c3c;
+  }
+  padding: 8px 0px;
+`
+
+const Role = styled.a`
+  color: rgb(189, 147, 249);
+`
+
+const Location = styled.span`
+  color: rgb(139, 233, 253);
+`
+
+const SectionHeader = styled.div`
+  margin-top: 25px;
+  padding: 10px 0px;
+  display: flex;
+  justify-content: space-between;
+  cursor: pointer;
+
+  &:hover {
+    background: #3c3c3c;
+  }
+`
+
+const SectionToggle = styled(SectionTitle)`
+  cursor: pointer;
+`
+
+export default class Section extends Component {
+  state = {
+    showDetails: false,
+  }
+
+  toggleSection = () => {
+    this.setState({ showDetails: !this.state.showDetails })
+  }
+
+  componentDidMount = () => {
+    this.setState({ showDetails: this.props.showDefault })
+  }
+
+  render() {
+    const { title, data } = this.props
+    const { showDetails } = this.state
+    return (
+      <section>
+        <SectionHeader onClick={this.toggleSection}>
+          <SectionTitle>{title}</SectionTitle>
+          <SectionToggle>{showDetails ? '[-]' : '[+]'}</SectionToggle>
+        </SectionHeader>
+        {showDetails && (
+          <ListContainer>
+            {data.map(role => (
+              <ListItem key={`${role.title}`}>
+                <Role href={`${role.link}`} target="_blank">
+                  {role.title}
+                </Role>
+              </ListItem>
+            ))}
+          </ListContainer>
+        )}
+      </section>
+    )
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import Layout from '../components/layout'
 // import Image from '../components/image'
 import SEO from '../components/seo'
 import Section from '../components/Section'
+import WritingSection from '../components/WritingSection'
 
 // data
 import ROLES from '../data/roles'
 import COMMUNITY from '../data/community'
+import WRITTING from '../data/writing'
 
 const IndexPage = () => (
   <Layout>
@@ -17,6 +19,7 @@ const IndexPage = () => (
       keywords={['john', 'pham', 'riverside', 'software', 'engineer']}
     />
     <Section title="Technical" data={ROLES} showDefault />
+    <WritingSection title="Writing" data={WRITTING} showDefault />
     <Section title="Community" data={COMMUNITY} />
   </Layout>
 )
