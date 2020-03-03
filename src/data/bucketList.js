d4712ce27b9b29e7e750180d9a1567c9714d272e
John Pham <johnp@microsoft.com>
2020-03-02 17:20:02 -0800

    Add initial bucket list

---
+++ src/data/bucketList.js
@@ -0,0 +1,68 @@
+/**
+ * {
+ *  name: string, coupled with url
+ *  url: string
+ *  progress: string
+ *  _html: string, an override for what's rendered
+ *  isDone: boolean
+ * }
+ */
+export default [
+  {
+    name: 'Travel to all 7 continents',
+    progress: '3/7',
+    url: null,
+    _html: null,
+    isDone: false,
+  },
+  {
+    name: 'Pass the Test of Chinese as a Foreign Language (TOCFL)',
+    progress: null,
+    url: null,
+    _html: null,
+    isDone: false,
+  },
+  {
+    name: 'Hike Mt. Rainier',
+    progress: null,
+    url: null,
+    _html: null,
+    isDone: false,
+  },
+  {
+    name: 'Run a marathon',
+    progress: null,
+    url: null,
+    _html: null,
+    isDone: false,
+  },
+  {
+    name: 'Read 1000 books',
+    progress: '38/1000',
+    url: null,
+    _html: null,
+    isDone: false,
+  },
+  {
+    name: 'Be vegan for a month+',
+    progress: null,
+    url: null,
+    _html: null,
+    isDone: false,
+  },
+  {
+    name: null,
+    progress: '615/1000',
+    url:
+      'https://www.oswego.edu/fitness-centers/sites/www.oswego.edu.fitness-centers/files/1000_-_500_lb_rules.pdf',
+    _html: 'Join the <del>500</del>1000 Pound Club',
+    isDone: false,
+  },
+  {
+    name: 'Buy a house',
+    progress: null,
+    url: null,
+    _html: null,
+    isDone: false,
+  },
+]
