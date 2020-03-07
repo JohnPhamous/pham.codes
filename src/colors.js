84ca1d8e67fde48fc28c5edae39d7d2507c48b3b
John Pham <johnphammail@gmail.com>
2020-03-07 14:41:47 -0800

    Add bucket list page

---
+++ src/colors.js
@@ -0,0 +1,7 @@
+export const green = 'rgb(51, 255, 51)'
+export const red = 'rgb(255, 51, 51)'
+export const gray = 'rgb(185, 180, 172)'
+export const onHoverGray = 'rgb(60, 60, 60)'
+export const purple = 'rgb(189, 147, 249)'
+export const blue = 'rgb(139, 233, 254)'
+export const comment = 'rgb(185, 180, 172)'
