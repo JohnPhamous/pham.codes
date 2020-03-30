8cbb476a871e697f84f0e01ea47d14c1198faff0
John Pham <johnphammail@gmail.com>
2020-03-29 19:56:46 -0700

    Initial draft of uses page

---
+++ src/pages/uses.js
@@ -0,0 +1,332 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const UsesPage = () => (
+  <Layout>
+    <SEO
+      title="Uses"
+      keywords={['john', 'pham', 'uses', 'software', 'engineer']}
+    />
+
+    <p>
+      Here's all of the things I use in a daily basis. Full disclosure, all
+      Amazon links are affiliate links.
+    </p>
+
+    <h2>
+      <span role="img" aria-label="laptop icon">
+        💻
+      </span>{' '}
+      Hardware
+    </h2>
+    <ul>
+      <li>Personal machine: 2018 15" MacBook Pro</li>
+      <li>Work Windows machine: HP Z2 G4</li>
+      <li>Work Linux machine: HP Z440</li>
+      <li>Work laptop: Surface Laptop 3</li>
+      <li>Google Pixel 3 XL</li>
+      <li>
+        <a
+          href="https://amzn.to/2vYQkzZ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sennheiser HD 598 Special Edition
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2WTI2nS"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Schiit Modi DAC
+        </a>{' '}
+        and{' '}
+        <a
+          href="https://amzn.to/3av4Zlc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Schiit Magni AMP
+        </a>
+      </li>
+      <li>2 Google Home Max for stereo</li>
+      <li>
+        <a
+          href="https://amzn.to/2QPnxVJ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {' '}
+          Happy Hacking Keyboard Professional 2
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/3dEb2pF"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Logitech MX Master 3 Mouse
+        </a>{' '}
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2wLzepz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          FitBit Ionic Smart Watch
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://ouraring.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Oura Smart Ring
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.upliftdesk.com/uplift-v2-standing-desk-v2-or-v2-commercial/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Uplift V2 Standing Desk
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2xuBP7s"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Dell 34" Ultrawide Monitor
+        </a>
+      </li>
+    </ul>
+
+    <h2>
+      <span role="img" aria-label="clothing icon">
+        👕
+      </span>{' '}
+      Clothing
+    </h2>
+
+    <h2>
+      <span role="img" aria-label="nutrition icon">
+        🍎
+      </span>{' '}
+      Nutrition
+    </h2>
+
+    <ul>
+      <li>
+        <a
+          href="https://amzn.to/3bM6pbB"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Creatine Monohydrate
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2UtjcK7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Omega 3 Fish Oil
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/3bxi25Z"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Magnesium L Threonate
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2Jm8f6v"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tumeric
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2w1wNyQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vitamin D3
+        </a>{' '}
+        5000 IU
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2WO8U8W"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Caffeine and L-Theanine
+        </a>{' '}
+        100 mg and 200 mg
+      </li>
+    </ul>
+
+    <h2>
+      <span role="img" aria-label="utilities icon">
+        🛠
+      </span>{' '}
+      Utility
+    </h2>
+
+    <ul>
+      <li>
+        <a
+          href="https://amzn.to/2wLzepz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Kleen Kanteen Water Bottle 27 oz
+        </a>
+      </li>
+    </ul>
+
+    <h2>
+      <span role="img" aria-label="plant icon">
+        🌱
+      </span>{' '}
+      Plant Family
+    </h2>
+
+    <ul>
+      <li>
+        <a
+          href="https://amzn.to/3armX8g"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Snake plants
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/3dGJdNk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Lots and lots of Pothos/Philodendrons
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2ydzjTe"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Rubber trees
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2JmXocD"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ZZ plants
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/2UH38Dk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Money trees (the organic ones)
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/3dFP59S"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aglaonema Chinese Evergreens
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://amzn.to/39vwvhc"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Air plants
+        </a>
+      </li>
+    </ul>
+
+    <h2>
+      <span role="img" aria-label="software icon">
+        👨‍💻
+      </span>{' '}
+      Software
+    </h2>
+
+    <ul>
+      <li>
+        Editor: VS Code with the{' '}
+        <a
+          href="https://marketplace.visualstudio.com/items?itemName=ahmadawais.shades-of-purple"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Shades of Purple
+        </a>{' '}
+        color scheme, the{' '}
+        <a href="https://dank.sh/" target="_blank" rel="noopener noreferrer">
+          Dank Mono
+        </a>{' '}
+        font, and the{' '}
+        <a
+          href="https://marketplace.visualstudio.com/items?itemName=vscodevim.vim"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Vim plugin
+        </a>
+      </li>
+      <li>Shell: zsh, Bash, CMD, and PowerShell</li>
+      <li>
+        Browser:{' '}
+        <a
+          href="https://www.microsoft.com/en-us/edge"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Microsoft Edge
+        </a>{' '}
+        and{' '}
+        <a
+          href="https://www.mozilla.org/en-US/firefox/developer/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Firefox Developer Edition
+        </a>
+      </li>
+    </ul>
+  </Layout>
+)
+
+export default UsesPage
