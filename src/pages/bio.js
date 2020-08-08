1ced6b6c20999d21bf5aa040a2d1df09d1b9a83e
John Pham <johnphammail@gmail.com>
2020-08-08 14:53:10 -0700

    Format bio page

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -5,11 +5,20 @@ import SEO from '../components/seo'
 
 const BioPage = () => (
   <Layout>
-    <SEO title="Biography" keywords={['john', 'pham', 'biography', 'speaker']} />
+    <SEO
+      title="Biography"
+      keywords={['john', 'pham', 'biography', 'speaker']}
+    />
     <h1>Bio</h1>
 
-    <p>John is a Software Engineer at Microsoft. He's worked for several other places, including NASA Jet Propulsion Laboratory, AT&T, and Amazon, and he's had the privilege of working with small businesses and academic institutions as a freelancer. He's experienced with delivering delightful features and leading projects from ideation to general audience. His passion is in customer discovery, user experience, and design systems.</p>
-
+    <p>
+      John is a Software Engineer at Microsoft. He's worked for several other
+      places, including NASA Jet Propulsion Laboratory, AT&T, and Amazon, and
+      he's had the privilege of working with small businesses and academic
+      institutions as a freelancer. He's experienced with delivering delightful
+      features and leading projects from ideation to general audience. His
+      passion is in customer discovery, user experience, and design systems.
+    </p>
   </Layout>
 )
 
