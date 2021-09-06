bba001bdcc7ef26076cd99697dc1e017e2790e9b
John Pham <john@highlight.run>
2021-09-06 11:53:34 -0700

    Update apple media to use newer names

--- components/layouts/vim/VimLayout.tsx
+++ components/layouts/vim/VimLayout.tsx
@@ -32,42 +32,42 @@ const VimLayout: React.FC<Props> = ({ title = 'John Pham', children }) => {
         <title>{title}</title>
 
         <link
-          rel="apple-touch-icon-precomposed"
+          rel="apple-touch-icon"
           sizes="57x57"
           href="/images/favicon/apple-touch-icon-57x57.png"
         />
         <link
-          rel="apple-touch-icon-precomposed"
+          rel="apple-touch-icon"
           sizes="114x114"
           href="/images/favicon/apple-touch-icon-114x114.png"
         />
         <link
-          rel="apple-touch-icon-precomposed"
+          rel="apple-touch-icon"
           sizes="72x72"
           href="/images/favicon/apple-touch-icon-72x72.png"
         />
         <link
-          rel="apple-touch-icon-precomposed"
+          rel="apple-touch-icon"
           sizes="144x144"
           href="/images/favicon/apple-touch-icon-144x144.png"
         />
         <link
-          rel="apple-touch-icon-precomposed"
+          rel="apple-touch-icon"
           sizes="60x60"
           href="/images/favicon/apple-touch-icon-60x60.png"
         />
         <link
-          rel="apple-touch-icon-precomposed"
+          rel="apple-touch-icon"
           sizes="120x120"
           href="/images/favicon/apple-touch-icon-120x120.png"
         />
         <link
-          rel="apple-touch-icon-precomposed"
+          rel="apple-touch-icon"
           sizes="76x76"
           href="/images/favicon/apple-touch-icon-76x76.png"
         />
         <link
-          rel="apple-touch-icon-precomposed"
+          rel="apple-touch-icon"
           sizes="152x152"
           href="/images/favicon/apple-touch-icon-152x152.png"
         />
@@ -88,6 +88,8 @@ const VimLayout: React.FC<Props> = ({ title = 'John Pham', children }) => {
         <meta name="msapplication-square150x150logo" content="/images/favicon/mstile-150x150.png" />
         <meta name="msapplication-wide310x150logo" content="/images/favicon/mstile-310x150.png" />
         <meta name="msapplication-square310x310logo" content="/images/favicon/mstile-310x310.png" />
+
+        <meta name="theme-color" content="#bd93f9" />
       </Head>
       <div>
         <VimHeader />
