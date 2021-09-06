c3925125092629caf36f9c29c113e67235616aa7
John Pham <john@highlight.run>
2021-09-06 14:18:39 -0700

    Add blog preview

---
+++ components/BlogPreview/BlogPreview.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import React from 'react';
+import { PostType } from '../../types/blog';
+import styles from './BlogPreview.module.css';
+import Image from 'next/image';
+
+interface Props {
+  post: PostType;
+}
+
+const BlogPreview = ({ post }: Props) => {
+  console.log(post);
+  return (
+    <article key={post.slug} className={styles.article}>
+      <h1>
+        <Link as={`/blog/${post.slug}`} href={`/blog/[slug]`}>
+          <a>{post.title}</a>
+        </Link>
+      </h1>
+      {post.image && <Image src={post.image} alt="" width="800px" height="250px" />}
+      <p>{post.description}</p>
+      <p>
+        <Link as={`/blog/${post.slug}`} href={`/blog/[slug]`}>
+          <a>Read More</a>
+        </Link>
+      </p>
+    </article>
+  );
+};
+
+export default BlogPreview;
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,6 @@
-import { format, parseISO } from 'date-fns';
 import type { GetStaticProps, NextPage } from 'next';
-import Link from 'next/link';
 import React from 'react';
+import BlogPreview from '../components/BlogPreview/BlogPreview';
 import { getAllPosts } from '../lib/api';
 import { PostType } from '../types/blog';
 
@@ -9,35 +8,23 @@ type BlogPageProps = {
   posts: PostType[];
 };
 
-const Home: NextPage = ({ posts }: BlogPageProps) => {
+// @ts-expect-error
+const Blog: NextPage = ({ posts }: BlogPageProps) => {
   return (
     <div>
       {posts.map((post) => (
-        <article key={post.slug} className="mt-12">
-          <p className="">{format(parseISO(post.date), 'MMMM dd, yyyy')}</p>
-          <h1 className="mb-2 text-xl">
-            <Link as={`/blog/${post.slug}`} href={`/blog/[slug]`}>
-              <a className="text-gray-900 dark:text-white dark:hover:text-blue-400">{post.title}</a>
-            </Link>
-          </h1>
-          <p className="mb-3">{post.description}</p>
-          <p>
-            <Link as={`/blog/${post.slug}`} href={`/blog/[slug]`}>
-              <a>Read More</a>
-            </Link>
-          </p>
-        </article>
+        <BlogPreview post={post} key={post.title} />
       ))}
     </div>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = getAllPosts(['date', 'description', 'slug', 'title']);
+  const posts = getAllPosts(['date', 'description', 'slug', 'title', 'image']);
 
   return {
     props: { posts },
   };
 };
 
-export default Home;
+export default Blog;
