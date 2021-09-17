0295feaa1c12993125556192924bf4ca174d8fc1
John Pham <john@highlight.run>
2021-09-16 22:10:29 -0700

    Add liveblocks

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { useOthers, useUpdateMyPresence } from '@liveblocks/react';
+import React, { useEffect } from 'react';
 import Projects from '../Projects/Projects';
 import Roles from '../Roles/Roles';
 import styles from './Home.module.css';
 
 const Home = () => {
+  const cursors = useWindowLiveCursors();
+
   return (
     <>
       <section className={styles.section}>
@@ -43,8 +46,116 @@ const Home = () => {
 
         <Projects />
       </section>
+      {cursors.map(({ x, y, connectionId }) => (
+        <Cursor key={connectionId} color={COLORS[connectionId % COLORS.length]} x={x} y={y} />
+      ))}
     </>
   );
 };
 
 export default Home;
+
+function useWindowLiveCursors() {
+  const updateMyPresence = useUpdateMyPresence();
+
+  useEffect(() => {
+    let scroll = {
+      x: window.scrollX,
+      y: window.scrollY,
+    };
+
+    let lastPosition: null | { x: number; y: number } = null;
+
+    function transformPosition(point: any) {
+      return {
+        x: point.x / window.innerWidth,
+        y: point.y,
+      };
+    }
+
+    function onPointerMove(event: any) {
+      const position = {
+        x: event.pageX,
+        y: event.pageY,
+      };
+      lastPosition = position;
+      updateMyPresence({
+        cursor: transformPosition(position),
+      });
+    }
+
+    function onPointerLeave() {
+      lastPosition = null;
+      updateMyPresence({ cursor: null });
+    }
+
+    function onDocumentScroll() {
+      if (lastPosition) {
+        const offsetX = window.scrollX - scroll.x;
+        const offsetY = window.scrollY - scroll.y;
+        const position = {
+          x: lastPosition?.x + offsetX,
+          y: lastPosition?.y + offsetY,
+        };
+        lastPosition = position;
+        updateMyPresence({
+          cursor: transformPosition(position),
+        });
+      }
+      scroll.x = window.scrollX;
+      scroll.y = window.scrollY;
+    }
+
+    document.addEventListener('scroll', onDocumentScroll);
+    document.addEventListener('pointermove', onPointerMove);
+    document.addEventListener('pointerleave', onPointerLeave);
+
+    return () => {
+      document.removeEventListener('scroll', onDocumentScroll);
+      document.removeEventListener('pointermove', onPointerMove);
+      document.removeEventListener('pointerleave', onPointerLeave);
+    };
+  }, [updateMyPresence]);
+
+  const others = useOthers();
+
+  return others
+    .toArray()
+    .filter((user) => user.presence?.cursor != null)
+    .map(({ connectionId, presence, id, info }) => {
+      return {
+        x: presence?.cursor.x * window.innerWidth,
+        y: presence?.cursor.y,
+        connectionId,
+        id,
+        info,
+        presence,
+      };
+    });
+}
+
+function Cursor({ color, x, y }: { color: string; x: number; y: number }) {
+  return (
+    <svg
+      style={{
+        position: 'absolute',
+        left: 0,
+        top: 0,
+        transition: 'transform 0.5s cubic-bezier(.17,.93,.38,1)',
+        transform: `translateX(${x}px) translateY(${y}px)`,
+      }}
+      width="24"
+      height="36"
+      viewBox="0 0 24 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.65376 12.3673H5.46026L5.31717 12.4976L0.500002 16.8829L0.500002 1.19841L11.7841 12.3673H5.65376Z"
+        fill={color}
+      />
+    </svg>
+  );
+}
+
+const COLORS = ['#DC2626', '#D97706', '#059669', '#7C3AED', '#DB2777'];
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,20 +2,39 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import VimLayout from '../components/layouts/vim/VimLayout';
 import { H } from 'highlight.run';
+import { createClient } from '@liveblocks/client';
+import { LiveblocksProvider, RoomProvider } from '@liveblocks/react';
+import React from 'react';
+
+const client = createClient({
+  authEndpoint: '/api/auth',
+});
 
 if (process.env.NODE_ENV !== 'development') {
   H.init('wneyvxe4', {
     networkRecording: {
-      recordHeadersAndBody: true
-    }
+      recordHeadersAndBody: true,
+    },
   });
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <VimLayout title="John Pham">
-      <Component {...pageProps} />
-    </VimLayout>
+    <LiveblocksProvider client={client}>
+      <RoomProvider
+        id={'example-live-cursors'}
+        /**
+         * Initialize the cursor position to null when joining the room
+         */
+        defaultPresence={() => ({
+          cursor: null,
+        })}
+      >
+        <VimLayout title="John Pham">
+          <Component {...pageProps} />
+        </VimLayout>
+      </RoomProvider>
+    </LiveblocksProvider>
   );
 }
 export default MyApp;
