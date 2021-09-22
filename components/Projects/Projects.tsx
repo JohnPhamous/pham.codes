64cebdc0df3451687c399713ca9214b5d721ee21
John Pham <johnphammail@gmail.com>
2021-09-21 20:55:59 -0700

    Add barbell project

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -3,19 +3,28 @@ import projects from '../../content/projects.content.json';
 import styles from './Projects.module.css';
 import Comment from '../Comment/Comment';
 import List from '../List/List';
+import Link from 'next/link';
 
 const Projects = () => {
   return (
     <List>
-      {projects.map((project) => (
-        <li className={styles.item} key={project.name}>
-          <a href={project.demo} target="_blank" rel="noopener noreferrer">
-            {project.name}
-          </a>
-          {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
-          <Comment>// {project.description}</Comment>
-        </li>
-      ))}
+      {projects.map((project) =>
+        project.link ? (
+          <li className={styles.item} key={project.name}>
+            <Link href={project.link}>{project.name}</Link>
+            {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
+            <Comment>// {project.description}</Comment>
+          </li>
+        ) : (
+          <li className={styles.item} key={project.name}>
+            <a href={project.demo} target="_blank" rel="noopener noreferrer">
+              {project.name}
+            </a>
+            {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
+            <Comment>// {project.description}</Comment>
+          </li>
+        )
+      )}
     </List>
   );
 };
