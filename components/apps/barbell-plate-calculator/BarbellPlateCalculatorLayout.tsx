3b9254d7dbad92f25204cb245ab70b2877eab710
John Pham <johnphammail@gmail.com>
2021-09-21 20:22:49 -0700

    Update font

--- components/apps/barbell-plate-calculator/BarbellPlateCalculatorLayout.tsx
+++ components/apps/barbell-plate-calculator/BarbellPlateCalculatorLayout.tsx
@@ -27,9 +27,9 @@ const BarbellPlateCalculatorLayout: React.FC = ({ children }) => {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         {/*  eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;700&display=swap"
           rel="stylesheet"
-        />
+        ></link>
       </Head>
       <div>
         <main>{children}</main>
