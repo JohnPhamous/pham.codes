cff11571af697186f6c044201c1ff2fa8d86687f
John Pham <johnphammail@gmail.com>
2022-05-16 19:22:51 -0700

    Remove liveblocks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import VimLayout from '../components/layouts/vim/VimLayout';
-import { createClient } from '@liveblocks/client';
-import { LiveblocksProvider, RoomProvider } from '@liveblocks/react';
 import React from 'react';
-
-const client = createClient({
-  authEndpoint: '/api/auth',
-});
 
 function MyApp({ Component, pageProps }: AppProps) {
   // @ts-expect-error
@@ -17,21 +11,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   }
 
   return (
-    <LiveblocksProvider client={client}>
-      <RoomProvider
-        id={'example-live-cursors'}
-        /**
-         * Initialize the cursor position to null when joining the room
-         */
-        defaultPresence={() => ({
-          cursor: null,
-        })}
-      >
-        <VimLayout title="John Pham">
-          <Component {...pageProps} />
-        </VimLayout>
-      </RoomProvider>
-    </LiveblocksProvider>
+    <VimLayout title="John Pham">
+      <Component {...pageProps} />
+    </VimLayout>
   );
 }
 export default MyApp;
