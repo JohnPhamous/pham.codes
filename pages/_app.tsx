151e67a70cc373be7ca2fa1b52ae4217ecb838c3
John Pham <johnphammail@gmail.com>
2023-02-16 21:40:05 +0700

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import VimLayout from '../components/layouts/vim/VimLayout';
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // @ts-expect-error
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <VimLayout title="John Pham">
       <Component {...pageProps} />
+      <Analytics />
     </VimLayout>
   );
 }
